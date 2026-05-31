851b98501421d1b950a01b9807d6520a344bcf27
Anandbd09 <anandbdundappanavar@gmail.com>
2026-05-31 17:58:25 +0530

    add embedding-service project structure and application.yml

---
+++ embedding-service/src/test/java/com/docintel/embedding_service/EmbeddingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docintel.embedding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
